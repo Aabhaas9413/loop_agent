fc3d10016ae204d9d2952cd92612e2ff286d482b
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2026-08-24 00:53:35 +0100

    CODE: Adding reviewer/verifier to access the response of the agent

--- backend.py
+++ backend.py
@@ -10,6 +10,16 @@ MAX_REVISIONS = int(os.getenv("MAX_REVISIONS", 2))
 
 model = OpenAI(model_name=MODEL, temperature=0.7, max_tokens=2000, api_key=os.getenv("OPENAI_KEY"))
 
+class Review(BaseModel):
+    decision: Literal["Pass", "Revise"] = Field(
+         description="PASS only of the answer satisfy the review rule; othervise REVISE."
+         )
+    feedback: str = Field(
+         description="Short, specific feedback. Empty string when decision is PASS."
+         )
+
+reviewer_model = model.with_structured_output(Review)    
+
 class State(TypedDict):
     topic: str
     draft: str
@@ -38,3 +48,22 @@ def writer(state: State) -> State:
         "decision": "",
         "revision_count": 0,
     }
+
+def reviewer(state: State):
+    """Agent 2: reviews the draft and provides feedback."""
+    review = reviewer_model.invoke(
+        [
+            {
+                "role": "system",
+                "content": "You are a helpful beginner friendly teacher, explain the topic in 150 to 160 words. Use simple language"
+            },
+            {
+                "role": "user",
+                "content": f"Review the following topic: {state['topic']} and provide feedback for both the question and the answer: {state['draft']}",
+            },
+        ]
+    )
+    return {
+        "decision": review.desicion,
+        "feedback": review.feedback
+    }    
